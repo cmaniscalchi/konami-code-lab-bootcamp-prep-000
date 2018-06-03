4da2e0ed49772780f9f4e19b21984767f29e6e42
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-03 23:44:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,6 +12,24 @@ const codes = [
 ];
 
 function init() {
+  
+  for (let i = 0, l = codes.length; i < l; i++) {
+    triggerKeyDown(codes[i]);
+    }
+      expect(spy.called).to.equal(true)
+      expect(spy.callCount).to.equal(1)
+    });
+
+    it("does not trigger an alert if the wrong code is entered", () => {
+      spy.reset()
+
+      for (let i = 0; i < codes.length - 1; i++) {
+        triggerKeyDown(codes[i])
+      }
+      triggerKeyDown("ArrowUp")
+      
+      expect(spy.notCalled).to.equal(true);
+    });
 
 for (let i = 0; i < codes.length - 1; i++) {
   return triggerKeyDown(codes[i]);
