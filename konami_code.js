399128f64b38f3b92eeb6412f97951b627e77eb0
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-03 23:37:35 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -13,27 +13,7 @@ const codes = [
 
 function init() {
   // your code here
-}
-spy.reset()
 for (let i = 0; i < codes.length - 1; i++) {
-  triggerKeyDown(codes[i])
+  return triggerKeyDown(codes[i])
 }
-triggerKeyDown("ArrowUp")
-
-expect(spy.notCalled).to.equal(true);
-
-function onKeyDownHandler(e) {
-  const key = e.key;
- 
-  if (key === alphabet[index]) {
-    index++;
- 
-    if (index === alphabet.length) {
-      alert("Hurray!");
- 
-      index = 0;
-    }
-  } else {
-    index = 0;
-  }
 }
