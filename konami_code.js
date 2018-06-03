78e6a7ead784aadeb5d2bae4791ca25907680cc7
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-03 23:41:14 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -14,15 +14,6 @@ const codes = [
 function init() {
   // your code here
 for (let i = 0; i < codes.length - 1; i++) {
-  return triggerKeyDown(codes[i])
+  return triggerKeyDown(codes[i]);
 }
-}
-
-function capture(e) {
-  console.log(this.firstChild.nodeValue.trim() + ' captured');
-}
- 
-for (let i = 0; i < codes.length; i++) {
- 
-  return triggerKeyDown (codes[i]).addEventListener('click', capture, true);
 }
