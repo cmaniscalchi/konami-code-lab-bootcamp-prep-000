7af78091ae14fce5acaccdca0dab0ef2b47bf2ea
Claudia Maniscalchi <claudia.maniscalchi@gmail.com>
2018-06-04 00:04:34 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -10,17 +10,3 @@ const codes = [
   "b",
   "a"
 ];
-
-function init() {
-  
-  let index = 0;
-  
-  document.addEventListener('keyDown', function(e)) {
-  alert('I was clicked!')
-};
- 
- 
-  for (let i = 0; i < codes.length - 1; i++) {
-    index.push(codes[i]);
-}
-}
